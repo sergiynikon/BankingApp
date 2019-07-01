99e3a92bc85569c8603105034378b83b128c1098
Serhii <sergiy.nikon2014@gmail.com>
2019-07-01 17:39:42 +0300

    Add authorize feature

---
+++ BankingApp/BankingApp/Services/Helpers/AuthOptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.IO;
+
+namespace Services.Helpers
+{
+    public class AuthOptions
+    {
+        //TODO: move all configurations to appsettings.json and add it using configuration
+
+        public const string Issuer = "BankingApp";
+        public const string Audience = "https://localhost:4000";
+
+        private const string Key = "ethuasb-ythu2-3uhs4ithukl3sth2--elk";
+
+        public const int Lifetime = 1;
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key));
+        }
+
+    }
+}
