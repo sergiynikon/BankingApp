ab506801035e4a286aafd4cd62859c6a1c2247e2
Serhii <sergiy.nikon2014@gmail.com>
2019-07-08 15:15:51 +0300

    DRY for BankingOperation & resolve inf loop
    infinite loop was when DbConcurencyException occured

--- BankingApp.Services/Interfaces/IBankingLogicService.cs
+++ BankingApp.Services/Interfaces/IBankingLogicService.cs
@@ -5,8 +5,8 @@ namespace BankingApp.Services.Interfaces
 {
     public interface IBankingLogicService
     {
-        ResultDto Deposit(Guid senderUserId, double amount);
-        ResultDto Withdraw(Guid senderUserId, double amount);
-        ResultDto Transfer(Guid senderUserId, Guid receiverUserId, double amount);
+        ResultDto Deposit(Guid senderUserId, OperationModelDto model);
+        ResultDto Withdraw(Guid senderUserId, OperationModelDto model);
+        ResultDto Transfer(Guid senderUserId, OperationModelDto model);
     }
 }
