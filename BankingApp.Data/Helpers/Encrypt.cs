b7d425a18fcce0cd33c1c1793e3b851c12f2c18a
Serhii <sergiy.nikon2014@gmail.com>
2019-07-09 14:16:15 +0300

    Improve readability in Encrypt file
    add more readable names and add comment

--- BankingApp.Data/Helpers/Encrypt.cs
+++ BankingApp.Data/Helpers/Encrypt.cs
@@ -7,16 +7,17 @@ namespace BankingApp.Data.Helpers
     {
         public static string GetHash(string input)
         {
-            using (MD5CryptoServiceProvider mdS = new MD5CryptoServiceProvider())
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
             {
-                byte[] b = Encoding.UTF8.GetBytes(input);
-                b = mdS.ComputeHash(b);
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hash = md5.ComputeHash(inputBytes);
 
                 StringBuilder sb = new StringBuilder();
 
-                foreach (byte x in b)
+                foreach (byte b in hash)
                 {
-                    sb.Append(x.ToString("x2"));
+                    //format to hexadecimal string with length at least 2
+                    sb.Append(b.ToString("X2"));
                 }
 
                 return sb.ToString();
