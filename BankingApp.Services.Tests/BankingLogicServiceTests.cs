cad96eafda415fc2aab9b4d0c357caf270649643
Serhii <sergiy.nikon2014@gmail.com>
2019-07-15 13:04:27 +0300

    Add tests

---
+++ BankingApp.Services.Tests/BankingLogicServiceTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using BankingApp.Data.Entities;
+using BankingApp.Data.UnitOfWork;
+using BankingApp.DataTransfer;
+using BankingApp.Services.Implementation;
+using BankingApp.Services.Interfaces;
+using Moq;
+using Xunit;
+
+namespace BankingApp.Services.Tests
+{
+    public class BankingLogicServiceTests
+    {
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        private readonly Guid _testSenderUserId = Guid.NewGuid();
+        private readonly Guid _testReceiverUserId = Guid.NewGuid();
+
+        private readonly ResultDto correctResultDto = ResultDto.Success();
+
+        public BankingLogicServiceTests()
+        {
+            _unitOfWorkMock.Setup(u => u.UserRepository.GetById(It.IsAny<Guid>()))
+                .Returns((User) null);
+
+            _unitOfWorkMock.Setup(u => u.UserRepository.GetById(It.Is<Guid>(id => id == _testSenderUserId)))
+                .Returns(GetTestSenderUser());
+
+            _unitOfWorkMock.Setup(u => u.UserRepository.GetById(It.Is<Guid>(id => id == _testReceiverUserId)))
+                .Returns(GetTestReceiverUser());
+
+            _unitOfWorkMock.Setup(u => u.TransactionRepository.Add(It.IsAny<Transaction>()));
+        }
+
+        #region Tests
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(double.MaxValue)]
+        [InlineData(0)]
+        public void Deposit_WithValidOperationModelDto_ReturnsSuccessResultDto(double amount)
+        {
+            //Arrange
+            var service = GetBankingLogicService();
+
+            //Act
+            var result = service.Deposit(_testSenderUserId, new OperationModelDto
+            {
+                Amount = amount
+            });
+
+            //Assert
+            Assert.True(result.IsSuccess);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-10)]
+        public void Deposit_WithInvalidOperationModelDto_ReturnsErrorResultDto(double amount)
+        {
+            //Arrange
+            var service = GetBankingLogicService();
+
+            //Act
+            var result = service.Deposit(_testSenderUserId, new OperationModelDto
+            {
+                Amount = amount
+            });
+
+            //Assert
+            Assert.False(result.IsSuccess);
+        }
+
+        #endregion
+
+        #region TestData
+
+        private IBankingLogicService GetBankingLogicService() => new BankingLogicService(_unitOfWorkMock.Object);
+
+        private User GetTestSenderUser() => new User("testSenderUser1", "testSenderUser1@gmail.com", "testSenderUser1Password");
+        private User GetTestReceiverUser() => new User("testReceiverUser1", "testReceiverUser1@gmail.com", "testReceiverUser1Password");
+
+        #endregion
+    }
+}
