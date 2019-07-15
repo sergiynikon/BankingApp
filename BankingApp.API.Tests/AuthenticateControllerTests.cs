efa1cb374e721f32195288b10fcc7745a64c6ca4
Serhii <sergiy.nikon2014@gmail.com>
2019-07-15 14:31:04 +0300

    Update AuthenticateControllerTests.cs

--- BankingApp.API.Tests/AuthenticateControllerTests.cs
+++ BankingApp.API.Tests/AuthenticateControllerTests.cs
@@ -15,24 +15,29 @@ namespace BankingApp.API.Tests
 
         private readonly Mock<IAuthenticateService> _authenticateServiceMock = new Mock<IAuthenticateService>();
 
-        private readonly LoginDto _validLoginModel = new LoginDto
+        private readonly LoginDto _invalidLoginModel = new LoginDto();
+
+        private readonly LoginDto _correctLoginModel = new LoginDto
         {
-            Login = "TestUserLogin1", Password = "TestUserPassword1"
+            Login = "TestUserLogin1",
+            Password = "TestUserPassword1"
         };
 
-        private readonly LoginDto _invalidLoginModel = new LoginDto
+        private readonly LoginDto _incorrectLoginModel = new LoginDto
         {
-            Login = "", Password = ""
+            Login = "TestUserLogin2", Password = "TestUserPassword2"
         };
 
-        private readonly RegisterDto _validRegisterDto = new RegisterDto
+        private readonly RegisterDto _invalidRegisterDto = new RegisterDto();
+
+        private readonly RegisterDto _correctRegisterDto = new RegisterDto
         {
             Login = "TestUserLogin1", Email = "TestUserLoin1@gmail.com", Password = "abc123", ConfirmPassword = "abc123"
         };
 
-        private readonly RegisterDto _invalidRegisterDto = new RegisterDto
+        private readonly RegisterDto _incorrectRegisterDto = new RegisterDto
         {
-            Login = "", Email = "", Password = "", ConfirmPassword = ""
+            Login = "TestUserLogin2", Email = "TestUserLogin2@gmail.com", Password = "abc12", ConfirmPassword = "abc12"
         };
 
         private readonly ResultDto _validResultDto = ResultDto.Success(
@@ -45,42 +50,56 @@ namespace BankingApp.API.Tests
 
         public AuthenticateControllerTests()
         {
-            _authenticateServiceMock.Setup(service => 
-                    service.GetIdentityToken(It.IsAny<LoginDto>()))
-                .Returns(_invalidResultDto);
-
-            _authenticateServiceMock.Setup(service => 
-                    service.GetIdentityToken(It.Is<LoginDto>(model => model.Equals(_validLoginModel))))
+            _authenticateServiceMock.Setup(service =>
+                    service.GetIdentityToken(It.Is<LoginDto>(model => model.Equals(_correctLoginModel))))
                 .Returns(_validResultDto);
 
-            _authenticateServiceMock.Setup(service => 
-                    service.RegisterUser(It.IsAny<RegisterDto>()))
+            _authenticateServiceMock.Setup(service =>
+                    service.GetIdentityToken(It.Is<LoginDto>(model => model.Equals(_incorrectLoginModel))))
                 .Returns(_invalidResultDto);
 
             _authenticateServiceMock.Setup(service =>
-                    service.RegisterUser(It.Is<RegisterDto>(m => m.Equals(_validRegisterDto))))
+                    service.RegisterUser(It.Is<RegisterDto>(m => m.Equals(_correctRegisterDto))))
                 .Returns(_validResultDto);
+
+            _authenticateServiceMock.Setup(service =>
+                    service.RegisterUser(It.Is<RegisterDto>(m => m.Equals(_incorrectRegisterDto))))
+                .Returns(_invalidResultDto);
         }
 
         #region LoginTests
+
         [Fact]
-        public void Login_WhenValidLoginDto_ReturnsOk()
+        public void Login_WhenModelStateIsValidAndModelIsCorrect_ReturnsOk()
         {
             //Arrange
-            var controller = GetController();
+            var controller = GetAuthenticateController();
 
             //Act
-            var result = controller.Login(_validLoginModel);
+            var result = controller.Login(_correctLoginModel);
 
             //Assert
             Assert.IsType<OkObjectResult>(result);
         }
 
         [Fact]
-        public void Login_WhenInvalidLoginDto_ReturnsBadRequest()
+        public void Login_WhenModelStateIsValidAndModelIsIncorrect_ReturnsBadRequest()
         {
             //Arrange
-            var controller = GetController();
+            var controller = GetAuthenticateController();
+
+            //Act
+            var result = controller.Login(_incorrectLoginModel);
+
+            //Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public void Login_WhenModelStateIsNotValid_ReturnsBadRequest()
+        {
+            //Arrange
+            var controller = GetAuthenticateController();
 
             //Act
             var result = controller.Login(_invalidLoginModel);
@@ -90,92 +109,55 @@ namespace BankingApp.API.Tests
         }
 
         [Fact]
-        public void Login_WhenModelStateIsValid_ReturnsOk()
+        public void Login_WhenCorrectLoginDto_ReturnsTokenInResultDto()
         {
             //Arrange
-            var controller = GetController();
+            var controller = GetAuthenticateController();
 
             //Act
-            var result = controller.Login(_validLoginModel);
-
-            //Assert
-            Assert.IsType<OkObjectResult>(result);
-        }
-
-        [Fact]
-        public void Login_WhenModelStateIsNotValid_ReturnsBadRequest()
-        {
-            //Arrange
-            var controller = GetController(addModelError: true);
-
-            //Act
-            var result = controller.Login(_validLoginModel);
-
-            //Assert
-            Assert.IsType<BadRequestObjectResult>(result);
-        }
-
-        [Fact]
-        public void Login_WhenValidLoginDto_ReturnsTokenInResultDto()
-        {
-            //Arrange
-            var controller = GetController();
-
-            //Act
-            var result = controller.Login(_validLoginModel);
+            var result = controller.Login(_correctLoginModel);
             var okResult = result as OkObjectResult;
             var resultDto = okResult?.Value;
 
             //Assert
             Assert.Equal(_validResultDto, resultDto);
         }
+
         #endregion
 
         #region RegisterTests
+
         [Fact]
-        public void Register_WhenModelStateIsValid_ReturnsOk()
+        public void Register_WhenModelStateIsValidAndModelIsCorrect_ReturnsOk()
         {
             //Arrange
-            var controller = GetController();
+            var controller = GetAuthenticateController();
 
             //Act
-            var result = controller.Register(_validRegisterDto);
+            var result = controller.Register(_correctRegisterDto);
 
             //Assert
             Assert.IsType<OkObjectResult>(result);
         }
 
         [Fact]
-        public void Register_WhenModelStateIsNotValid_ReturnsBadRequest()
+        public void Register_WhenModelStateIsValidAndModelIsIncorrect_ReturnsBadRequest()
         {
             //Arrange
-            var controller = GetController(addModelError: true);
+            var controller = GetAuthenticateController();
 
             //Act
-            var result = controller.Register(_validRegisterDto);
+            var result = controller.Register(_incorrectRegisterDto);
 
             //Assert
             Assert.IsType<BadRequestObjectResult>(result);
         }
 
         [Fact]
-        public void Register_WhenValidRegisterDto_ReturnsOk()
+        public void Register_WhenModelStateIsNotValid_ReturnsBadRequest()
         {
             //Arrange
-            var controller = GetController();
-
-            //Act
-            var result = controller.Register(_validRegisterDto);
-
-            //Assert
-            Assert.IsType<OkObjectResult>(result);
-        }
-
-        [Fact]
-        public void Register_WhenInvalidRegisterDto_ReturnsBadRequest()
-        {
-            //Arrange
-            var controller = GetController();
+            var controller = GetAuthenticateController();
 
             //Act
             var result = controller.Register(_invalidRegisterDto);
@@ -183,20 +165,15 @@ namespace BankingApp.API.Tests
             //Assert
             Assert.IsType<BadRequestObjectResult>(result);
         }
+
         #endregion
 
         #region TestData
-        private AuthenticateController GetController(bool addModelError = false)
+        private AuthenticateController GetAuthenticateController()
         {
-            var controller = new AuthenticateController(_authenticateServiceMock.Object);
-
-            if (addModelError)
-            {
-                controller.ModelState.AddModelError("someKey", "someError");
-            }
-
-            return controller;
+            return new AuthenticateController(_authenticateServiceMock.Object);
         }
+
         #endregion
     }
 }
