b869735325c5d86267a9f9ea7e92a4370cc54fd2
sergiynikon <sergiy.nikon2014@gmail.com>
2019-07-17 13:54:39 +0300

    Add Swagger to BankingApp.API (#58)
    * add swagger to API
    
    * Change SwaggerDoc Title
    
    Rename to BankingApp.API and use const Version

---
+++ BankingApp.API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace BankingApp.API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        private const string Version = "v1.0";
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(Version, new Info { Title = $"BankingApp.API {Version}", Version = Version });
+
+                // Swagger 2.+ support
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[] { }},
+                };
+
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+                c.AddSecurityRequirement(security);
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{Version}/swagger.json", $"Versioned API {Version}");
+
+                c.DocumentTitle = "Title Documentation";
+                c.DocExpansion(DocExpansion.None);
+            });
+
+            return app;
+        }
+    }
+}
