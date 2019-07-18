79ff76d5e4e3099ab7d3e0ddf348323de9c0fa8b
sergiynikon <sergiy.nikon2014@gmail.com>
2019-07-18 14:43:23 +0300

    Revert "Add Swagger to API (#60)"
    This reverts commit 03406bdabacf905066249c41e61058cd98ae2141.

--- BankingApp.API/Extensions/SwaggerServiceExtensions.cs
+++ BankingApp.API/Extensions/SwaggerServiceExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Swashbuckle.AspNetCore.Swagger;
