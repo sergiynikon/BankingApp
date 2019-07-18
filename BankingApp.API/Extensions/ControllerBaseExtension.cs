6a3c917dc4881cf85ec5c5b51e0340e2cd12b20e
Serhii <sergiy.nikon2014@gmail.com>
2019-07-18 15:28:55 +0300

    Change Claims type from Name to Id

--- BankingApp.API/Extensions/ControllerBaseExtension.cs
+++ BankingApp.API/Extensions/ControllerBaseExtension.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Linq;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BankingApp.API.Extensions
 {
     public static class ControllerBaseExtension
     {
+        private static readonly string DefaultClaimsType = "Id";
         public static Guid GetCurrentUserId(this ControllerBase controller)
         {
-            return Guid.Parse(controller.User.Claims.First(c => c.Type == ClaimTypes.Name).Value);
+            return Guid.Parse(controller.User.Claims.Single(c => c.Type == DefaultClaimsType).Value);
         }
     }
 }
