49a1332fc0ba75241da9860b2d3f910854ff5e58
Serhii <sergiy.nikon2014@gmail.com>
2019-07-18 15:34:42 +0300

    Add Id to userViewModelDto

--- BankingApp.DataTransfer/UserViewModelDto.cs
+++ BankingApp.DataTransfer/UserViewModelDto.cs
@@ -1,10 +1,12 @@
-﻿using BankingApp.Data.Entities;
+﻿using System;
+using BankingApp.Data.Entities;
 using BankingApp.DataTransfer.Helpers;
 
 namespace BankingApp.DataTransfer
 {
     public class UserViewModelDto
     {
+        public Guid Id { get; set; }
         public string Login { get; set; }
         public string Email { get; set; }
         public double Balance { get; set; }
@@ -12,6 +14,7 @@ namespace BankingApp.DataTransfer
         public static UserViewModelDto ConvertFromUser(User user) =>
             new UserViewModelDto()
             {
+                Id = user.Id,
                 Login = user.Login,
                 Email = user.Email,
                 Balance = Casting.LongToDouble(user.Balance)
